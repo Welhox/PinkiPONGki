82801a2aeaf285b52f192f8b1c0bd26984c2cc24
Ryan Boudwin <47835762+KrolPolski@users.noreply.github.com>
2025-06-19 15:14:03 +0300

    Set up tournament structure for frontend, apply automated linting rules for the project
    * Added choose play mode component
    
    * now rendering player input fields based on number of players
    
    * added submit button and wrapped the input elements in a form
    
    * Fixed typescript error by defining props data type in the TournamentBuilder component
    
    * Applied automated linting rules using prettier

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -10,6 +10,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Stats from "./pages/Stats";
 import VerifyEmail from "./pages/VerifyEmail";
 import TournamentPage from "./pages/TournamentPage";
+import ResetPassword from './pages/ResetPassword';
 import showDatabase from "./components/showDatabase";
 import NavigationHeader from "./components/NavigationHeader";
 import { useAuth } from "./auth/AuthProvider";
@@ -53,6 +54,7 @@ const App: React.FC = () => {
         <Route path="/stats" element={<Stats />} />
         <Route path="/mfa" element={<Mfa />} />
         <Route path="/tournament/:id" element={<TournamentPage />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route
           path="/stats/:anything"
