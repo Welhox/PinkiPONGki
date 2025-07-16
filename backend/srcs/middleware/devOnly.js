bbf0f1263ac51b62db77905adbb1b0eba96452a5
Casimir Lundberg <clundber@student.hive.fi>
2025-07-16 15:43:22 +0300

    fixed linting errors

--- backend/srcs/middleware/devOnly.js
+++ backend/srcs/middleware/devOnly.js
@@ -1,9 +1,9 @@
 
 //This is a prehandler for restricting APIs to localhost only
 export async function devOnly(request, reply) {
-	  console.log("running devOnly middleware");
-	// Allow access only from localhost
-	if (request.ip !== "::1" && request.ip !== "127.0.0.1") {
-	  return reply.code(403).send({ error: "Access denied. This endpoint is only available from localhost." });
-	}
+  console.log("running devOnly middleware");
+  // Allow access only from localhost
+  if (request.ip !== "::1" && request.ip !== "127.0.0.1") {
+    return reply.code(403).send({ error: "Access denied. This endpoint is only available from localhost." });
+  }
 }
